97760f3d37f3a0a06576dc5cedc70efd289e1bd7
Roush,Kyle <Kyle.Roush@cerner.com>
2019-02-12 12:07:21 -0600

    forgoten children

---
+++ excuses.go
@@ -0,0 +1,11 @@
+package main
+
+func listExcuses() map[string]string {
+
+	return map[string]string{
+		"dog":      "My dog ate my homework.",
+		"notSorry": "not sorry I just don't wanna.",
+		"posion":   "I cant make cause the food I eat is trying to kill me.",
+		"toliet":   "I won't be there because I can't get off the toliet.",
+	}
+}
