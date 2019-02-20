0c6017d929961a6368c4ad57304891b53165f737
Roush,Kyle <Kyle.Roush@cerner.com>
2019-02-20 11:28:39 -0600

    Updates

---
+++ precache-manifest.b26a611f5f70001244bba432bedf845a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/eaas/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2c1c7ab80bc98cd600be",
+    "url": "/eaas/static/js/runtime~main.2c1c7ab8.js"
+  },
+  {
+    "revision": "3dbe22441f013d4dd5d4",
+    "url": "/eaas/static/js/main.3dbe2244.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/eaas/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "3dbe22441f013d4dd5d4",
+    "url": "/eaas/static/css/main.f56201a3.chunk.css"
+  },
+  {
+    "revision": "906e09acf0ca66f6d9061089c776545f",
+    "url": "/eaas/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.40cc0edeedaa23eccfd915d109f49d6e.js"
+  "/eaas/precache-manifest.b26a611f5f70001244bba432bedf845a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/eaas/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
